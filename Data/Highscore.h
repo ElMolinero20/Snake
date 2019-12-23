14ea7d9309cf06365a71bdd9ee88241aa693cde6
joblxck <51477596+joblxck@users.noreply.github.com>
2019-12-23 18:11:05 +0100

    changes

--- Data/Highscore.h
+++ Data/Highscore.h
@@ -7,5 +7,9 @@ class Highscore
     public:
         Highscore();
         ~Highscore();
-        void DataRead();
+        void ReadData();
+        void WriteHighscore(std::string name, int score);
+        void Split(const std::string& s, char c, std::vector<std::string>& v);
+        std::vector<std::string> names;
+        std::vector<int> scores;
 };
