383eca05c4f3b1b2e994ba6983690697cc9400ca
joblxck <51477596+joblxck@users.noreply.github.com>
2019-12-23 14:57:01 +0100

    changes

---
+++ Data/Highscore.cpp
@@ -0,0 +1,21 @@
+#include "Highscore.h"
+
+Highscore::Highscore()
+{
+
+}
+
+Highscore::~Highscore()
+{
+
+}
+
+void Highscore::DataRead()
+{
+    std::ifstream highscore("Highscore.txt");
+
+    if(highscore.is_open())
+    {
+        
+    }
+}
---
+++ Data/Highscore.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class Highscore
+{
+    public:
+        Highscore();
+        ~Highscore();
+        void DataRead();
+};
